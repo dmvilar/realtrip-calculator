b964e54409aa495adca87d2be5c9fbe314a1ce99
dmvilar <daniel_martins_vilar@outlook.com>
2023-08-28 18:09:28 -0300

    ajuste manifest 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,45 +1,45 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
+import { getManifest } from 'workbox-build';
 // https://vitejs.dev/config/
 
 const manifestForPlugin: Partial<VitePWAOptions> = {
-  registerType: "prompt",
-  includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
-  manifest: {
-    theme_color: "#f69435",
-    background_color: "#f69435",
-    display: "browser",
-    scope: "/",
-    start_url: "/",
-    name: "RealTrip Calculadora",
-    short_name: "RealTrip",
-    icons: [
-      {
-        src: "/icon-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        src: "/icon-256x256.png",
-        sizes: "256x256",
-        type: "image/png",
-      },
-      {
-        src: "/icon-384x384.png",
-        sizes: "384x384",
-        type: "image/png",
-      },
-      {
-        src: "/icon-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-      },
-    ],
-  }
+	registerType: "prompt",
+	manifest: {
+    "theme_color": "#f69435",
+    "background_color": "#f69435",
+    "display": "standalone",
+    "scope": "/",
+    "start_url": "/",
+    "name": "RealTrip Calculadora",
+    "short_name": "RealTrip",
+    "icons": [
+        {
+            "src": "/icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+        },
+        {
+            "src": "/icon-256x256.png",
+            "sizes": "256x256",
+            "type": "image/png"
+        },
+        {
+            "src": "/icon-384x384.png",
+            "sizes": "384x384",
+            "type": "image/png"
+        },
+        {
+            "src": "/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+        }
+    ]
+},
 };
 
 export default defineConfig({
-  base: "./",
-  plugins: [react(), VitePWA(manifestForPlugin)],
+	base: "./",
+	plugins: [react(), VitePWA(manifestForPlugin)],
 });
