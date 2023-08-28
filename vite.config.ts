c80e48e8c806a863fa7d173306de392465109f07
dmvilar <daniel_martins_vilar@outlook.com>
2023-08-28 18:56:01 -0300

    ajuste

--- vite.config.ts
+++ vite.config.ts
@@ -1,71 +1,43 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
 
 const manifestForPlugin: Partial<VitePWAOptions> = {
-  registerType: "prompt",
-  manifest: {
-    theme_color: "#000000",
-    background_color: "#000000",
-    name: "RealTrip Calculadora",
-    short_name: "RealTrip",
-    display: "standalone",
-    scope: "./",
-    start_url: "./",
-    icons: [
-      {
-        src: "assets/icons/icon-72x72.png",
-        sizes: "72x72",
-        type: "image/png",
-        purpose: "maskable any",
-      },
-      {
-        src: "assets/icons/icon-96x96.png",
-        sizes: "96x96",
-        type: "image/png",
-        purpose: "maskable any",
-      },
-      {
-        src: "assets/icons/icon-128x128.png",
-        sizes: "128x128",
-        type: "image/png",
-        purpose: "maskable any",
-      },
-      {
-        src: "assets/icons/icon-144x144.png",
-        sizes: "144x144",
-        type: "image/png",
-        purpose: "maskable any",
-      },
-      {
-        src: "assets/icons/icon-152x152.png",
-        sizes: "152x152",
-        type: "image/png",
-        purpose: "maskable any",
-      },
-      {
-        src: "assets/icons/icon-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-        purpose: "maskable any",
-      },
-      {
-        src: "assets/icons/icon-384x384.png",
-        sizes: "384x384",
-        type: "image/png",
-        purpose: "maskable any",
-      },
-      {
-        src: "assets/icons/icon-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "maskable any",
-      },
-    ],
-  },
+	registerType: "prompt",
+	manifest: {
+    "theme_color": "#000000",
+    "background_color": "#000000",
+    "display": "standalone",
+    "scope": "/",
+    "start_url": "/",
+    "name": "RealTrip Calculadora",
+    "short_name": "RealTrip",
+    "icons": [
+        {
+            "src": "/icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+        },
+        {
+            "src": "/icon-256x256.png",
+            "sizes": "256x256",
+            "type": "image/png"
+        },
+        {
+            "src": "/icon-384x384.png",
+            "sizes": "384x384",
+            "type": "image/png"
+        },
+        {
+            "src": "/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+        }
+    ]
+},
 };
 
 export default defineConfig({
-  base: "/",
-  plugins: [react(), VitePWA(manifestForPlugin)],
+	base: "/",
+	plugins: [react(), VitePWA(manifestForPlugin)],
 });
