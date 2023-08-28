3cca63ab46238174b915cd2d430aff7b4e820cfa
dmvilar <daniel_martins_vilar@outlook.com>
2023-08-28 17:57:19 -0300

    ajustes no manifest

--- vite.config.ts
+++ vite.config.ts
@@ -7,28 +7,28 @@ const manifestForPlugin: Partial<VitePWAOptions> = {
 	registerType: "prompt",
 	includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
 	manifest: {
-		name: "Weather Ups",
-		short_name: "Weathe Ups",
-		description: "An app that can show weather forecast for your city.",
+		name: "RealTrip - Calculadora",
+		short_name: "RealTrip",
+		description: "Calculadora de custo de passagens.",
 		icons: [
 			{
-				src: "/android-chrome-192x192.png",
+				src: "/src/assets/realtriplogo.png",
 				sizes: "192x192",
 				type: "image/png",
 			},
 			{
-				src: "/android-chrome-512x512.png",
+				src: "/src/assets/realtriplogo.png",
 				sizes: "512x512",
 				type: "image/png",
 			},
 			{
-				src: "/apple-touch-icon.png",
+				src: "/src/assets/realtriplogo.png",
 				sizes: "180x180",
 				type: "image/png",
 				purpose: "apple touch icon",
 			},
 			{
-				src: "/maskable_icon.png",
+				src: "/src/assets/realtriplogo.png",
 				sizes: "225x225",
 				type: "image/png",
 				purpose: "any maskable",
