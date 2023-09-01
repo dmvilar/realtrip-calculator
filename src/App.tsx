3abff78010b5689a8c1be5a9c47d7e8661398e87
dmvilar <daniel_martins_vilar@outlook.com>
2023-09-01 14:05:12 -0300

    ajustes visuais

--- src/App.tsx
+++ src/App.tsx
@@ -96,7 +96,7 @@ function App() {
         </Box>
       )}
 
-      <Box position="relative" paddingTop="10" paddingBottom="10">
+      <Box position="relative" marginTop={mostrarVolta ? "6rem" : "3rem"} marginBottom={"8rem"}>
         <Divider borderColor="green" width={"100%"} />
         <AbsoluteCenter bg="white" border={"solid 1px green"} px="4">
           <Text>{"Custo total:"}</Text>
